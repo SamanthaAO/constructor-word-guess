d1f8415a6377aa88751f0bc40817d696e7be842c
SamanthaAO <samanthaaorcutt@gmail.com>
2019-06-10 19:08:44 -0700

    created function to pick a random word at the begining of each game

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var Word = require("./word.js")
 var inquirer = require('inquirer');
 
 
-var words = ["PINEAPPLE", "CHERRY", "COCONUT"];
+var words = ["PINEAPPLE UPSIDE DOWN CAKE", "CHERRY PIE", "FROZEN YOGURT", "CHEESECAKE","ICE CREAM", "CHOCOLATE CHIP COOKIES", "FUDGE", "CHOCOLATE BROWNIE", "LADYFINGER", "BANANA PUDDING", "BLACKBERRY COBBLER", "APPLE CRISP"];
 var correctCounter;
 var displayStringVictory;
 //var letterInput = "";
 
 
-var game = new Word("CHERRY PIE");
+var game;
+
+wordChoice();
 game.addLetter();
 
 
@@ -56,8 +58,15 @@ function guessCorrect(){
     };
 }
 
+function wordChoice(){
+    var randomNum = Math.floor(Math.random() * words.length);
+    console.log(words[randomNum]);
+    game = new Word(words[randomNum]) ;
+
+}
+
 function victory(){
-console.log(displayStringVictory.indexOf("_"));
+//console.log(displayStringVictory.indexOf("_"));
     if(displayStringVictory.indexOf("_") == -1){
         console.log("You got it correct!! Here is your next word")
         ///this will change to select new word and then play game
